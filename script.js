faeeeb4973e2697467f558a9339c3a2b8d35989c
03tb <132428099+03tb@users.noreply.github.com>
2023-12-26 19:06:37 +1100

    Add operator functions

--- script.js
+++ script.js
@@ -2,34 +2,123 @@ const container = document.querySelector('#container');
 
 const display = document.querySelector('#display');
 
-// method to return numbers pressed in maths class
+// simple calculation logic
 
-let currentValue = "";
-let numberValue;
-let parsedValue;
+let userInput;
+let displayValue;
+let result;
+
+function addFunction(userInput, displayValue) {
+    return userInput + displayValue;
+}
+
+function subtractFunction(userInput, displayValue) {
+    return userInput - displayValue;
+}
+
+function multiplyFunction(userInput, displayValue) {
+    return userInput * displayValue;
+}
+
+function divideFunction(userInput, displayValue) {
+    return userInput / displayValue;
+}
+
+function operate(type, userInput, displayValue) {
+
+    switch (type) {
+        case 'add':
+            result = addFunction(userInput, displayValue);
+            console.log(result);
+            break;
+        case 'subtract':
+            result = subtractFunction(userInput, displayValue);
+            console.log(result);
+            break;
+        case 'multiply':
+            result = multiplyFunction(userInput, displayValue);
+            console.log(result);
+            break;
+        case 'divide':
+            result = divideFunction(userInput, displayValue);
+            console.log(result);
+            break;
+    }
+}
+
+
+// display changing
+// need to implement input displayed as string in display like 7 + 7 + 7
+
+let tempValue = '';
+
+const currentInput = document.querySelector('#currentInput');
 
 const numberButton = document.querySelectorAll('.maths');
-const currentInput = document.querySelector('#currentInput');
 
 numberButton.forEach((button) => {
     button.addEventListener ('click', () => {
         const numberValue = button.getAttribute('data-number');
-        currentValue = currentValue.concat("", numberValue);
-        console.log(currentValue);
-        currentInput.textContent = currentValue;
+        tempValue = tempValue.concat("", numberValue);
+        displayValue = parseInt(tempValue);
+        currentInput.textContent = displayValue;
+        display.appendChild(currentInput);
     })
-    display.appendChild(currentInput);
 })
 
+// buttons implementation
 
+const operatorButton = document.querySelectorAll('.operators')
 
-function parseCurrentValue(currentValue) {
-    parsedValue = parseInt (currentValue)
+let type;
+
+const resultDisplay = document.querySelector('#result');
+
+operatorButton.forEach((button) => {
+    button.addEventListener ('click', () => {
+        if (button.textContent == "/") {
+            type = 'divide'
+            userInput = displayValue; 
+            tempValue = ""; //reset temp and display value for next string
+            displayValue ="";
+        } else if (button.textContent == "*") {
+            type = 'multiply'
+            userInput = displayValue; 
+            tempValue = ""; //reset temp and display value for next string
+            displayValue ="";
+        } else if (button.textContent == "-") {
+            type = 'subtract'
+            userInput = displayValue; 
+            tempValue = ""; //reset temp and display value for next string
+            displayValue ="";
+        } else if (button.textContent == "+") {
+            type = 'add'
+            userInput = displayValue; 
+            tempValue = ""; //reset temp and display value for next string
+            displayValue ="";
+        } else if (button.textContent == "=") {
+            displayValue = parseInt(displayValue);
+            operate(type, userInput, displayValue);
+            userInput = result;
+            displayValue = result;
+            tempValue = "";
+            
+        }
+        
+    })
+})
+
+function storeValue () {
+    operate(type, userInput, displayValue);
 }
 
-const equalsButton = document.querySelector('.equals');
 
-equalsButton.addEventListener('click', () => {  
-    parseCurrentValue(currentValue);
-    console.log('this is the parsed value' + parsedValue);
+// clear button
+
+const clearButton = document.querySelector('#clear');
+
+clearButton.addEventListener('click', () => {
+    currentValue = "";
+    currentInput.textContent = '0';
+    display.appendChild(currentInput);
 })
